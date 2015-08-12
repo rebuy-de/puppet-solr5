849772d6a1f69aa34df892fb1b78f88b894c954d
Daniel Freudenberger <df@rebuy.de>
2015-08-12 12:15:57 +0200

    use puppet-archive instead of download-uncompress

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,6 +1,5 @@
 class solr5::params {
     $package_url = 'http://ftp.halifax.rwth-aachen.de/apache/lucene/solr/5.2.1/solr-5.2.1.tgz'
-    $package_version = '5.2.1'
     $package_target_dir = '/usr/local/src'
     $solr_install_dir = '/opt'
     $solr_data_dir = '/var/solr'
